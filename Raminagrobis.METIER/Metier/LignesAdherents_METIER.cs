b508d9f84e881c916bd4ec89d3099651214d5026
SoGameHD <erwann.leducq@epsi.fr>
2022-03-08 00:14:05 +0100

    Suppression surgarche de code niveau de la couche metier #7

--- Raminagrobis.METIER/Metier/LignesAdherents_METIER.cs
+++ Raminagrobis.METIER/Metier/LignesAdherents_METIER.cs
@@ -18,42 +18,5 @@ namespace Raminagrobis.METIER.Metier
         public LignesAdherents_METIER(int id_ligne_global, int quantite) => (ID_ligne_global, Quantite) = (id_ligne_global, quantite);
 
         public LignesAdherents_METIER(int id_produit, int id_commande, int id_ligne_global, int quantite) => (ID_produit, ID_commande, ID_ligne_global, Quantite) = (id_produit, id_commande, id_ligne_global, quantite);
-
-        #region Insert
-        public void Insert()
-        {
-
-            LignesAdherents_DAL lignesAdherents = new LignesAdherents_DAL(ID_ligne_global, Quantite);
-
-            var depotAdherents = new LignesAdherentsDepot_DAL();
-
-            lignesAdherents = depotAdherents.Insert(lignesAdherents);
-
-            ID_produit = lignesAdherents.ID_produit;
-            ID_commande = lignesAdherents.ID_commande;
-        }
-        #endregion
-
-        #region Delete
-        public void Delete()
-        {
-            LignesAdherents_DAL lignesAdherents = new LignesAdherents_DAL(ID_ligne_global, Quantite);
-
-            var depotAdherents = new LignesAdherentsDepot_DAL();
-
-            depotAdherents.Delete(lignesAdherents);
-        }
-        #endregion
-
-        #region Update
-        public void Update()
-        {
-            LignesAdherents_DAL lignesAdherents = new LignesAdherents_DAL(ID_ligne_global, Quantite);
-
-            var depotAdherents = new LignesAdherentsDepot_DAL();
-
-            depotAdherents.Update(lignesAdherents);
-        }
-        #endregion
     }
 }
--- Raminagrobis.METIER/Metier/LignesGlobal_METIER.cs
+++ Raminagrobis.METIER/Metier/LignesGlobal_METIER.cs
@@ -17,38 +17,5 @@ namespace Raminagrobis.METIER.Metier
 
 
         public LignesGlobal_METIER(int id_panier, int quantite, int id_produit) => (ID_panier, Quantite, ID_produit) = (id_panier, quantite, id_produit);
-
-        #region Insert
-        public void Insert()
-        {
-            LignesGlobal_DAL LignesGlobal = new LignesGlobal_DAL(ID_panier, Quantite, ID_produit);
-
-            var depotLignesGlobal = new LignesGlobalDepot_DAL();
-
-            LignesGlobal = depotLignesGlobal.Insert(LignesGlobal);
-
-            ID = LignesGlobal.ID;
-        }
-        #endregion
-
-        #region Delete
-        public void Delete()
-        {
-            LignesGlobal_DAL LignesGlobal = new LignesGlobal_DAL(ID_panier, Quantite, ID_produit);
-
-            var depotLignesGlobal = new LignesGlobalDepot_DAL();
-            depotLignesGlobal.Delete(LignesGlobal);
-        }
-        #endregion
-
-        #region Update
-        public void Update()
-        {
-            LignesGlobal_DAL LignesGlobal = new LignesGlobal_DAL(ID_panier, Quantite, ID_produit);
-
-            var depotLignesGlobal = new LignesGlobalDepot_DAL();
-            depotLignesGlobal.Update(LignesGlobal);
-        }
-        #endregion
     }
 }
