f4ce5af7e3a5823a82c3fb4f4fba8675f645cddb
SoGameHD <erwann.leducq@epsi.fr>
2022-03-08 00:17:14 +0100

    Oublie #7

--- Raminagrobis.METIER/Metier/CommandeAdherents_METIER.cs
+++ Raminagrobis.METIER/Metier/CommandeAdherents_METIER.cs
@@ -16,36 +16,5 @@ namespace Raminagrobis.METIER.Metier
 
         public CommandeAdherents_METIER(int id_adherent, int id_panier) => (ID_adherent, ID_panier) = (id_adherent, id_panier);
         public CommandeAdherents_METIER(int id, int id_adherent, int id_panier) => (ID, ID_adherent, ID_panier) = (id, id_adherent, id_panier);
-
-        #region Insert
-        public void Insert()
-        {
-            CommandeAdherents_DAL CommandeAdherents = new(ID_adherent, ID_panier);
-
-            var depotAdherent = new CommandeAdherentsDepot_DAL();
-
-            CommandeAdherents = depotAdherent.Insert(CommandeAdherents);
-
-            ID = CommandeAdherents.ID;
-        }
-        #endregion
-
-        #region Delete
-        public void Delete()
-        {
-            CommandeAdherents_DAL CommandeAdherents = new CommandeAdherents_DAL(ID_adherent, ID_panier);
-            var depotfournisseur = new CommandeAdherentsDepot_DAL();
-            depotfournisseur.Delete(CommandeAdherents);
-        }
-        #endregion
-
-        #region Update
-        public void Update()
-        {
-            CommandeAdherents_DAL CommandeAdherents = new CommandeAdherents_DAL(ID_adherent, ID_panier);
-            var depotCommandeAdherents = new CommandeAdherentsDepot_DAL();
-            depotCommandeAdherents.Update(CommandeAdherents);
-        }
-        #endregion
     }
 }
